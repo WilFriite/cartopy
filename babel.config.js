fa8076eb57e11df5446cb70d208b17db15e9b0c4
WilFriite <jnguessan2003@gmail.com>
2025-08-19 16:22:08 +0200

    Add expo-sqlite and babel-plugin-inline-import dependencies; update metro config for SQL file support

--- babel.config.js
+++ babel.config.js
@@ -1,18 +1,16 @@
 module.exports = function (api) {
   api.cache(true);
-  let plugins = [];
-
-  plugins.push([
-    'react-native-unistyles/plugin',
-    {
-      autoProcessRoot: 'app',
-      autoProcessImports: ['~/components'],
-    },
-  ]);
-
   return {
     presets: ['babel-preset-expo'],
-
-    plugins,
+    plugins: [
+      ['inline-import', { extensions: ['.sql'] }],
+      [
+        'react-native-unistyles/plugin',
+        {
+          autoProcessRoot: 'app',
+          autoProcessImports: ['~/components'],
+        },
+      ],
+    ],
   };
 };
--- metro.config.js
+++ metro.config.js
@@ -5,4 +5,6 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
+config.resolver.sourceExts.push('sql');
+
 module.exports = config;
