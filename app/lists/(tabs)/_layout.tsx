68d1204719174daa05037418cab20ac0fece5758
WilFriite <jnguessan2003@gmail.com>
2025-08-20 12:21:09 +0200

    Refactor ListsPagesLayout: Remove unused imports and simplify tabBarStyle configuration for cleaner layout management.

--- app/lists/(tabs)/_layout.tsx
+++ app/lists/(tabs)/_layout.tsx
@@ -1,18 +1,12 @@
-import { Stack, Tabs } from 'expo-router';
-import { useUnistyles } from 'react-native-unistyles';
+import { Tabs } from 'expo-router';
 import { Icon } from '~/components/ui/icon';
 
 export default function ListsPagesLayout() {
-  const { theme, rt } = useUnistyles();
-
   return (
     <Tabs
       screenOptions={{
         headerShown: false,
         headerBackButtonDisplayMode: 'minimal',
-        tabBarStyle: {
-          backgroundColor: theme.colors.background,
-        },
       }}>
       <Tabs.Screen
         name="index"
