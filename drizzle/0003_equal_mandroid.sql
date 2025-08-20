d3624711c47a71b2976e555ab21b3c5e89f78bdc
WilFriite <jnguessan2003@gmail.com>
2025-08-20 15:57:49 +0200

    Add migration scripts for list table updates: Create new lists table, migrate data, and enforce unique name constraint.

---
+++ drizzle/0003_equal_mandroid.sql
@@ -0,0 +1,14 @@
+PRAGMA foreign_keys=OFF;--> statement-breakpoint
+CREATE TABLE `__new_lists` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`name` text NOT NULL,
+	`items` text DEFAULT '',
+	`created_at` text DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	`last_performed_at` text DEFAULT NULL
+);
+--> statement-breakpoint
+INSERT INTO `__new_lists`("id", "name", "items", "created_at", "last_performed_at") SELECT "id", "name", "items", "created_at", "last_performed_at" FROM `lists`;--> statement-breakpoint
+DROP TABLE `lists`;--> statement-breakpoint
+ALTER TABLE `__new_lists` RENAME TO `lists`;--> statement-breakpoint
+PRAGMA foreign_keys=ON;--> statement-breakpoint
+CREATE UNIQUE INDEX `lists_name_unique` ON `lists` (`name`);
