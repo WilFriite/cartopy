623812a4685ffc8009c7e70e26415cdb3c8e2fdf
WilFriite <jnguessan2003@gmail.com>
2025-08-20 17:28:06 +0200

    Implement DisplayListsPage component with live data fetching, rendering of list items in a FlatList, and enhanced UI using Card components.

--- app/lists/(tabs)/index.tsx
+++ app/lists/(tabs)/index.tsx
@@ -1,14 +1,54 @@
+import { useLiveQuery } from 'drizzle-orm/expo-sqlite';
+import { useCallback } from 'react';
+import { FlatList } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { Button, ButtonIcon, ButtonText } from '~/components/ui/btn';
+import { Card } from '~/components/ui/card';
+
 import { Container } from '~/components/ui/container';
 import { Text } from '~/components/ui/typography';
+import { lists, ListSelectType } from '~/db/schema';
+import { useDrizzle } from '~/hooks/use-drizzle';
+import { formatListItems } from '~/utils/format';
 
 export default function DisplayListsPage() {
+  const db = useDrizzle();
+  const { data } = useLiveQuery(db.select().from(lists));
+
+  const renderItem = useCallback((item: ListSelectType) => {
+    return (
+      <Card padding="lg">
+        <Card.Header>
+          <Card.Title>{item.name}</Card.Title>
+        </Card.Header>
+        <Card.Body>
+          <Text>{formatListItems(item.items)?.length} items</Text>
+        </Card.Body>
+        <Card.Footer>
+          <Button variant="outline">
+            <ButtonText>Voir plus</ButtonText>
+            <ButtonIcon name="arrow-right" />
+          </Button>
+        </Card.Footer>
+      </Card>
+    );
+  }, []);
+
   return (
-    <>
+    <GestureHandlerRootView style={{ flex: 1 }}>
       <Container>
         <Text size="xl" align="center">
           Mes Listes
         </Text>
+        <Text size="lg">{data?.length} listes.</Text>
+        <FlatList
+          data={data}
+          numColumns={1}
+          scrollEnabled={false}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({ item }) => renderItem(item)}
+        />
       </Container>
-    </>
+    </GestureHandlerRootView>
   );
 }
