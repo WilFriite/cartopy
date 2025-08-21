d8bfd083ed614def3776dc2f31fcc6a2161a4192
WilFriite <jnguessan2003@gmail.com>
2025-08-21 19:45:31 +0200

    Active by default header & deactivate it on default tabs router

--- app/lists/(tabs)/_layout.tsx
+++ app/lists/(tabs)/_layout.tsx
@@ -1,30 +1,33 @@
-import { Tabs } from 'expo-router';
+import { Stack, Tabs } from 'expo-router';
 import { Icon } from '~/components/ui/icon';
 import { TabBar } from '~/components/ui/tab-bar';
 
 export default function ListsPagesLayout() {
   return (
-    <Tabs
-      tabBar={(props) => <TabBar {...props} />}
-      screenOptions={{
-        headerShown: false,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Mes listes',
-          tabBarIcon: ({ focused }) => <Icon name="list" color={focused ? 'astral' : 'muted'} />,
-        }}
-      />
-      <Tabs.Screen
-        name="create"
-        options={{
-          title: 'Ajouter',
-          tabBarIcon: ({ focused }) => (
-            <Icon name="plus-circle" color={focused ? 'astral' : 'muted'} />
-          ),
-        }}
-      />
-    </Tabs>
+    <>
+      <Stack.Screen options={{ headerShown: false }} />
+      <Tabs
+        tabBar={(props) => <TabBar {...props} />}
+        screenOptions={{
+          headerShown: false,
+        }}>
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: 'Mes listes',
+            tabBarIcon: ({ focused }) => <Icon name="list" color={focused ? 'astral' : 'muted'} />,
+          }}
+        />
+        <Tabs.Screen
+          name="create"
+          options={{
+            title: 'Ajouter',
+            tabBarIcon: ({ focused }) => (
+              <Icon name="plus-circle" color={focused ? 'astral' : 'muted'} />
+            ),
+          }}
+        />
+      </Tabs>
+    </>
   );
 }
