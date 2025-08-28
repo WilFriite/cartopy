564125a18193979b954080b5e987336c07a21023
WilFriite <jnguessan2003@gmail.com>
2025-08-28 22:18:05 +0200

    Add KeyboardProvider

--- components/layout/providers.tsx
+++ components/layout/providers.tsx
@@ -1,11 +1,14 @@
 import { PropsWithChildren } from 'react';
 import { DbProvider } from './db-provider';
 import { QueryClientProvider } from './query-client-provider';
+import { KeyboardProvider } from 'react-native-keyboard-controller';
 
 export function Providers({ children }: PropsWithChildren) {
   return (
-    <DbProvider>
-      <QueryClientProvider>{children}</QueryClientProvider>
-    </DbProvider>
+    <KeyboardProvider>
+      <DbProvider>
+        <QueryClientProvider>{children}</QueryClientProvider>
+      </DbProvider>
+    </KeyboardProvider>
   );
 }
