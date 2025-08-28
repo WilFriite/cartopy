cdf8f552041640504858c1058a3012248faef11a
WilFriite <jnguessan2003@gmail.com>
2025-08-28 22:32:18 +0200

    Add keyboard handling to CreateListPage with KeyboardAvoidingView and custom hook for dynamic keyboard height

---
+++ hooks/use-kbd-height.ts
@@ -0,0 +1,21 @@
+import { useKeyboardHandler } from 'react-native-keyboard-controller';
+import { useSharedValue } from 'react-native-reanimated';
+
+const OFFSET = 42;
+
+export const useKbdHeight = () => {
+  const totalOffset = OFFSET;
+
+  const height = useSharedValue(totalOffset);
+
+  useKeyboardHandler(
+    {
+      onMove: (e) => {
+        'worklet';
+        height.value = e.height > 0 ? Math.max(e.height + OFFSET, totalOffset) : totalOffset;
+      },
+    },
+    []
+  );
+  return { height };
+};
